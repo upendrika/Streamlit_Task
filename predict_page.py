781413e177ad6ccb9d7478e8efd8ab70b58fe86e
ishara upendrika <145433182+upendrika@users.noreply.github.com>
2024-08-03 23:29:03 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -18,7 +18,7 @@ le_education = data["le_education"]
 
 def show_predict_page():
    
-    st.video("Software Developers Video.mp4")
+    st.image('salary pre.png')
     st.title("Software Developer Salary Prediction")
     
     st.sidebar.header(" We need some information to predict the salary")
